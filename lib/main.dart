3e7ad202405d0072473da25f54347c62234f405b
T-Dourgarian <thomasdourgarian@gmail.com>
2019-08-08 19:45:56 -0500

    flutter skeleton

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+import 'package:flutter/rendering.dart';
+import "dart:async";
+import "dart:convert";
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(
+    MaterialApp()
+  );
+}
+
